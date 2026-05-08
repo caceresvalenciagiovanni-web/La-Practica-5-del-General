5813aa3dd8eadecec499134fe2e6f70be92fe376
caceresvalenciagiovanni-web <caceres.valencia.giovanni@gmail.com>
2026-05-07 20:45:22 -0600

    Refactor Symbol and Datum structures with new prototypes
    Added function prototypes for install and execerror.

--- hoc.h
+++ hoc.h
@@ -1,4 +1,4 @@
-typedef struct Symbol { /* entrada en la tabla de símbolos */
+typedef struct Symbol { /* entrada en la tabla de sÃ­mbolos */
 char    *name;
 short   type;   /* VAR, BLTIN, UNDEF */
 union {
@@ -6,44 +6,47 @@ double val;	/* si es VAR */
 double  (*ptr)(double);       /* si es BLTIN */
 } u;
 struct Symbol   *next;  /* para ligarse a otro */ } Symbol;
-Symbol  *install(), *lookup();
+Symbol  *install(char *s, int t, double d);
+Symbol  *lookup(char *s);
 
-typedef union Datum {   /* tipo de la pila del intérprete */
+typedef union Datum {   /* tipo de la pila del intÃ©rprete */
 double  val;
 Symbol  *sym; } Datum; 
 
 extern Datum pop();
 
-typedef void (*Inst)(void);  /* instrucción de maquina */ 
+typedef void (*Inst)(void);  /* instrucciÃ³n de maquina */ 
 
 #define STOP    (Inst) 0
 
-extern	Inst prog[ ] , *progp, *code();
+extern	Inst prog[ ], *progp;
+extern	Inst *code(Inst f);
 
-extern	void eval();
-extern	void add();
-extern	void sub(); 
-extern	void mul(); 
-extern	void div(); 
-extern	void negate(); 
-extern	void power();
-extern	void assign(); 
-extern	void bltin(); 
-extern	void varpush(); 
-extern	void constpush();
-extern	void print();
-extern	void pop1();
+extern	void eval(void);
+extern	void add(void);
+extern	void sub(void); 
+extern	void mul(void); 
+extern	void div(void); 
+extern	void negate(void); 
+extern	void power(void);
+extern	void assign(void); 
+extern	void bltin(void); 
+extern	void varpush(void); 
+extern	void constpush(void);
+extern	void print(void);
+extern	void pop1(void);
 
-extern	void prexpr();
-extern	void gt();
-extern	void lt(); 
-extern	void eq(); 
-extern	void ge(); 
-extern	void le(); 
-extern	void ne(); 
-extern	void and(); 
-extern	void or();
-extern	void not();
-extern	void ifcode(); 
-extern	void whilecode();
+extern	void prexpr(void);
+extern	void gt(void);
+extern	void lt(void); 
+extern	void eq(void); 
+extern	void ge(void); 
+extern	void le(void); 
+extern	void ne(void); 
+extern	void and(void); 
+extern	void or(void);
+extern	void not(void);
+extern	void ifcode(void); 
+extern	void whilecode(void);
 void execute(Inst *p);
+void execerror(char *s, char *t);
